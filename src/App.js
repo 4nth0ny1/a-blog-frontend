e47d7855d37614200f716d5cd195c11c3b8fc9d6
4nth0ny1 <acatullo4@gmail.com>
2021-09-04 23:04:23 -0500

    add addTodo will change to add blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>A-Blog</h1>
-    </div>
-  );
-}
+class App extends Component {
 
-export default App;
+  state = {
+    todo: ''
+  }
+
+  addTodo = () => {
+    return ({
+      type: 'ADD_TODO',
+      todo: this.state.todo
+    })
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      todo: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    console.log("Todo being added: ", this.state.todo);
+    this.props.addTodo(this.state.todo);
+    this.setState({ todo: '' });
+  }
+
+  render() {
+    const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
+    return (
+      <div className="App">
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <input
+          type="text"
+          onChange={(event) => this.handleOnChange(event)}
+          id="todos"
+          placeholder="add todo" 
+          value={this.state.todo}/>
+        <input type="submit" />
+      </form>
+      <h2>Todos:</h2>
+        <ol>{renderTodos()}</ol>
+      </div>
+    );
+  }
+};
+
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
